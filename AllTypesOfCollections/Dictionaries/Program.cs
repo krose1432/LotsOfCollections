d994ac5684f4b7c5b2b23cccc4e66415ab185366
krose1432 <keishon.rose@ou.edu>
2021-08-03 14:52:08 -0500

    Updates

---
+++ AllTypesOfCollections/Dictionaries/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> COURSES = new Dictionary<string, string>();
+            Console.WriteLine("Enter three courses would you like to add seperated  by a comma");
+            string course = Console.ReadLine();
+           
+            Console.WriteLine("Enter the grades of the three course grades you entered earlier seperated by a comma");
+            string grades = Console.ReadLine();
+            for (int i = 0; i < 3; i++)
+            {
+                COURSES.Add(course.Split(",")[i], grades.Split(',')[i]);
+            }
+            COURSES.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
+
+
+
+
+
+        }
+    }
+}
